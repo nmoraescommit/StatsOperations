1b8dc58a40b704424079d2d0a2044f7390ae639e
nmoraescommit <nmoraes@ucsb.edu>
2024-08-09 14:28:12 -0700

    Initial commit

---
+++ statsop.R
@@ -0,0 +1,26 @@
+2 < 5
+5 < 2
+2 == 4
+2 != 4
+"n" > "M"
+"N" > "M"
+"cat" < "dog"
+dogs <- c(1, 10, 4, 5, 18)
+cats <- c(0, 12, 10, 5, 2)
+
+dogs < cats
+
+dog_names <- c("Teddy", "Khora", "Banjo", "Tallie")
+cat_names <- c("Henrietta", "Panda", "Merlin", "Runt")
+
+cat_names < dog_names
+
+2 <= 4
+4 >= 600
+5 < 10 & "cat" < "dog"
+bananas <- c(10, 21, 12, 15, 22)
+apples <- c(9, 18, 16, 20, 30)
+
+apples < bananas & bananas > 20
+5 > 6 | "dog" < "cat" | 10 < 50
+!"dog" == "cat"
